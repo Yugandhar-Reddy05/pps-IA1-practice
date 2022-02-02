c313c54c27f8bedfbdf67479b0d9db4e3fec004c
Yugandhar-Reddy05 <93091593+Yugandhar-Reddy05@users.noreply.github.com>
2022-02-02 14:57:42 +0530

    Update p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include <stdio.h>
+int input(int *a,int *b)
+{
+  printf("enter a,b values\n");
+  scanf("%d%d",a,b);
+}
+int add (int a,int b,int *c)
+{
+  *c=a+b;
+}
+void output(int c)
+{
+  printf("addition of a,b is%d",c);
+}
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output (z);
+  return 0;
 }
